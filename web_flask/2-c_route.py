ea019692aee8763c53ee74467fe8580d9944840c
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-06-05 04:00:16 -0700

    2-c_route.py edited

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web applicaion.
-
-The application listens on 0.0.0.0:5000
+"""Starts Flask web app
 Routes:
-    /: displays "Hello HBNB!"
-    /hbnb: displays "HBNB"
-    /c/<text>: display "C " followed by the value of the text
-    Variable (replace underscore _ symbol with space).
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
 """
-
-
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb_home():
-    """Displays 'Hello HBNB!'"""
+def hbnb_route():
+    """prints Hello HBNB"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb_route():
-    """Displays HBNB"""
+def hbnb():
+    """prints HBNB"""
     return "HBNB"
 
 
-@app.route('/hbnb/c/<text>', strict_slashes=False)
+@app.route('/c/<string:text>', strict_slashes=False)
 def c_text(text):
-    """
-    Displays 'C ' followed by the value of the text variable
-    """
-    text = text.replace('_', ' ')
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
     return "C %s" % text
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
