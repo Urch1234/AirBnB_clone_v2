12c907c0249ee451ff6db56c1279e990ce2d0bad
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-06-05 08:11:56 -0700

    6-number_odd_or_even.py edited

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,110 +1,63 @@
 #!/usr/bin/python3
-"""
-Starts Flask web app.
-
+"""Starts Flask web app
 Routes:
     / - display "Hello HBNB!"
     /hbnb - display "HBNB"
     /c/<text> - display "C <text>"
-    /python/<text> - display "Python <text>"
-    /number/<n> - display "n is a number" iff n is an integer
+    /python/<text> - display "Python is cool"
+    /number/<n> - display n if integer
     /number_template/<n> - display a HTML page if n is int
     /number_odd_or_even/<n> - displays if a number is odd or even
 """
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnbb_home():
-    """Displays 'Hello HBNB!"""
+def hbnb_route():
+    """prints Hello HBNB"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb_route():
-    """Display 'HBNB'"""
+def hbnb():
+    """prints HBNB"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<string:text>', strict_slashes=False)
 def c_text(text):
-    """
-    Display 'C ' followed by text content,
-    Replaces underscore in the text with spaces.
-
-    Args:
-        text (str): The text to display.
-
-    Returns:
-        str: The formatted string with underscore replaced by spacaes.
-    """
+    """prints C followed by <text> content"""
     text = text.replace("_", " ")
-    return "C {}".format(text)
+    return "C %s" % text
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    """
-    Displays 'Python ' followed by the content of the text.
-
-    Replaces underscores in the text with spaces.
-    Default text is 'is cool'.
-
-    Args:
-        text (str): The text to display, 'is cool' by default.
-
-    Returns:
-        str: The formatted string with underscores replacred by spaces.
-    """
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """prints Python is cool"""
     text = text.replace("_", " ")
-    return "Python {}".format(text)
+    return "Python %s" % text
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
-    """
-    Displays '<n> is a number' only if n is an integer.
-
-    Args:
-        n (int): The number to display.
-
-    Returns:
-        str: The formatted string confirming  n is a number.
-    """
-    return "{} is a number".format(n)
+    """displays n if integer"""
+    return "%i is a number" % n
 
 
-@app.route('/number_template/<n>',  strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """
-    Displays an HTML page only if n is an integer
-
-    Args:
-        n (int): The formatted string confirming n is a number.
-
-    Returns:
-        An HTML page if n is an integer.
-    """
+    """display a HTML page if n is int"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    """
-    displays an HTML page if n is an integer.
-
-    Args:
-        n (int): The formatted string confirming n is a number.
-
-    Returns:
-        An HTML page.
-    """
+    """display a HTML page if n is int"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
